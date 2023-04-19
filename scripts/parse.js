990c2de7d87846a28fe8bdba40a9d095609a2e2d
ddr4x <chen.shawn123@gmail.com>
2023-04-19 14:03:54 -0400

    add functionality to store info on weapon damage

--- scripts/parse.js
+++ scripts/parse.js
@@ -6,10 +6,18 @@ const rows = csv.split('\n').slice(1);
 
 //store a specifc column for now ill just do col 17 (weapon)
 const weapons = [];
+const damage = [];
+const armorDamage = [];
 
 rows.forEach(row => {
     const columns = row.split(',');
     weapons.push(columns[16]);
+    damage.push(columns[11]);
+    armorDamage.push(columns[12]);
+
   });
 
-  console.log(weapons);
+console.log(weapons);
+console.log(damage);
+console.log(armorDamage);
+  
