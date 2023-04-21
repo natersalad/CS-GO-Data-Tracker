af934a3f0103ab934ec8c4c45cee5a8d631a5f7e
Nathan Wand <nathan.wand1234@gmail.com>
2023-04-21 01:24:35 -0400

    created damage occurrence types

---
+++ scripts/DamageOccurrenceTypes.js
@@ -0,0 +1,31 @@
+//made a weapon class to hold all parameters about specific weapons (we can add onto this)
+export default class DamageOccurrence {
+    constructor(weaponName, hpDamage, armorDamage){
+        this.weaponName = weaponName;
+        this.hpDamage = hpDamage;
+        this.armorDamage = armorDamage;
+        this.totalDamage = hpDamage + armorDamage;
+    }
+
+}
+
+export class DamageOccurrenceEntries {
+    constructor(damageOccurrenceEntries){
+        this.damageOccurrenceEntries = damageOccurrenceEntries;
+    }
+
+    outputSortData(sortBy) {
+        //todo
+        return {
+            sortBy: sortBy,
+            sortedList: "some list soon",
+            mergeSortData: {
+                duration: "swoase",
+                memoryUsage: "swoasememory"
+            }
+        }
+    }
+        
+}
+
+
