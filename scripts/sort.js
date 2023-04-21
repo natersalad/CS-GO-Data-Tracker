09cea00179daa361eb8003c973a6b90af82d067c
ddr4x <chen.shawn123@gmail.com>
2023-04-21 11:47:10 -0400

    import damageoccurence object array into sort

--- scripts/sort.js
+++ scripts/sort.js
@@ -1,20 +1,24 @@
+import { DamageOccurrenceEntries } from "./DamageOccurrenceTypes.js";
+
 // bubble sort function
 export function bubbleSort(arr, sortBy) {
     let len = arr.length;
-    for (let i = len - 1; i >= 0; i--) {
-        for (let j = 1; j <= i; j++) {
-            if (arr[j - 1][sortBy] > arr[j][sortBy]) {
-                let temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
+    for (var i = 0; i < len-1; i++){
+        for (var j = 0; j < len - i - 1; j++){
+            if (arr[j][sortBy] > arr[j + 1][sortBy]) {
+                // swap
+                var temp = arr[j][sortBy];
+                arr[j][sortBy] = arr[j + 1][sortBy];
+                arr[j + 1][sortBy] = temp;
             }
+            
         }
     }
     return arr;
 }
 
 // call the function
-var sortedArray = bubbleSort([3, 0, 2, 5, -1, 4, 1]);
+var sortedArray = bubbleSort(DamageOccurrenceEntries, "weaponName");
 console.log(sortedArray);
 
 
