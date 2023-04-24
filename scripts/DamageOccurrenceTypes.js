c45a5eb88b09c21b42a44748a6c52402e28244f3
Nathan Wand <nathan.wand1234@gmail.com>
2023-04-24 02:56:44 -0400

    made csv files for every map, added hitbox.

--- scripts/DamageOccurrenceTypes.js
+++ scripts/DamageOccurrenceTypes.js
@@ -2,13 +2,13 @@ import { bubbleSort } from "./sort.js";
 
 //made a weapon class to hold all parameters about specific weapons (we can add onto this)
 export default class DamageOccurrence {
-    constructor(weaponName, hpDamage, armorDamage){
+    constructor(weaponName, hpDamage, armorDamage, hitbox){
         this.weaponName = weaponName;
         this.hpDamage = hpDamage;
         this.armorDamage = armorDamage;
+        this.hitbox = hitbox;
         this.totalDamage = hpDamage + armorDamage;
     }
-
 }
 
 export class DamageOccurrenceEntries {
@@ -16,71 +16,21 @@ export class DamageOccurrenceEntries {
         this.damageOccurrenceEntries = damageOccurrenceEntries;
     }
 
-    outputSortData(sortBy) {
+    outputSortData(sortBy, sort1, sort2) {
+
         let start = performance.now();
         let sortedList = bubbleSort(this.damageOccurrenceEntries, sortBy)
         let bubbleDuration = performance.now() - start;
 
-        // start = performance.now();
-        // intialMemoryUsage = performance.memory.usedJSHeapSize;
-        // mergeSort(this.damageOccurrenceEntries, sortBy)
-        // let mergeMemoryUsage = performance.memory.usedJSHeapSize - intialMemoryUsage;
-        // let mergeDuration = performance.now() - start;
-
-        // start = performance.now();
-        // intialMemoryUsage = performance.memory.usedJSHeapSize;
-        // insertionSort(this.damageOccurrenceEntries, sortBy)
-        // let insertionMemoryUsage = performance.memory.usedJSHeapSize - intialMemoryUsage;
-        // let insertionDuration = performance.now() - start;
-
-        // start = performance.now();
-        // intialMemoryUsage = performance.memory.usedJSHeapSize;
-        // selectionSort(this.damageOccurrenceEntries, sortBy)
-        // let selectionMemoryUsage = performance.memory.usedJSHeapSize - intialMemoryUsage;
-        // let selectionDuration = performance.now() - start;
-
-        // start = performance.now();
-        // intialMemoryUsage = performance.memory.usedJSHeapSize;
-        // quickSort(this.damageOccurrenceEntries, sortBy)
-        // let quickMemoryUsage = performance.memory.usedJSHeapSize - intialMemoryUsage;
-        // let quickDuration = performance.now() - start;
-
-        // start = performance.now();
-        // intialMemoryUsage = performance.memory.usedJSHeapSize;
-        // heapSort(this.damageOccurrenceEntries, sortBy)
-        // let heapMemoryUsage = performance.memory.usedJSHeapSize - intialMemoryUsage;
-        // let heapDuration = performance.now() - start;
-
         return {
+            sort1: sort1,
+            sort2: sort2,
             sortBy: sortBy,
             sortedList: sortedList,
             bubbleSortStats: {
                 duration: bubbleDuration,
             },
-            // mergeSortData: {
-            //     duration: "swoase",
-            //     memoryUsage: "swoasememory"
-            // },
-            // insertSortData: {
-            //     duration: "swoase",
-            //     memoryUsage: "swoasememory"
-            // },
-            // selectionSortData: {
-            //     duration: "swoase",
-            //     memoryUsage: "swoasememory"
-            // },
-            // quickSortData: {
-            //     duration: "swoase",
-            //     memoryUsage: "swoasememory"
-            // },
-            // heapSortData: {
-            //     duration: "swoase",
-            //     memoryUsage: "swoasememory"
-            // },
-            // bucketSortData: {
-            //     duration: "swoase",
-            //     memoryUsage: "swoasememory"
-            // }
+            
         }
     }
 }
